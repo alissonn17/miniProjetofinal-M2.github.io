e1307cf7e457b363a10ba59363a24fe3a1f0f713
Alisson <llielzinh@gmail.com>
2024-10-27 11:11:45 -0300

    :construction: metodo read

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
-const button = document.getElementById("botao")
+const button = document.getElementById("submit")
+const nome = document.getElementById("name")
+const sentence = document.getElementById("frase")
+const writeName = document.getElementById("spanName")
+const writeSentence = document.getElementById("spanFrase")
 
-button.addEventListener("click",()=>{alert("uau")})
+let user = []
+let result = []
+let resultAtual = []
+
+let i = 0
+
+
+class Frase{
+    constructor(nameUser, sentenceUser){
+        this.nome = nameUser.value
+        this.frase = sentenceUser.value
+    }
+    read(){
+        writeName.innerText = this.nome
+        writeSentence.innerText = this.frase
+        
+    }
+}
+
+function get(){
+    user.push(new Frase(nome,sentence))   
+    user[i].read()
+    resultAtual.push(writeName)
+    resultAtual.push(writeSentence)
+    result.push(resultAtual)
+
+    i += 1
+    
+}
+
+button.addEventListener("click",()=>{
+    get()
+})
+    
